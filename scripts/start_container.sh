f4ca8457b5bc8e118a078a203b4e5320cb44e37c
harinivash8 <hari.v8080@gmail.com>
2025-04-29 23:24:20 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-set -e
 
-# Stop any running container with the same name
-echo Stopping existing container if exists...
+echo "Stopping existing container if exists..."
 docker stop twig-tomcat || true
 docker rm twig-tomcat || true
 
-# Build and run the Docker container
-echo Running the new container...
-docker build -t twig-tomcat /home/ec2-user/
-docker run -d --name twig-tomcat -p 8080:8080 /home/ec2-user/twig-tomcat:latest
+echo "Running the new container..."
+cd /home/ec2-user/app || exit 1
+
+docker build -t twig-tomcat .
+docker run -d --name twig-tomcat -p 8080:8080 twig-tomcat
